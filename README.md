a081768eb74707830cb7fb6bbc85536d98fa18cc
fajartc02 <fajartc02@gmail.com>
2018-09-21 09:38:37 +0700

    express config

--- README.md
+++
@@ -1,1 +0,0 @@
-# test-lock-system-on-raspberry
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../firebase')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log('mausk bras');
+  console.log('mamam');
   
-  db.ref('user/').set({
-    apayangmauluset: 'set'
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(err => {
-    console.log(err);
-  })
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
